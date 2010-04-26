a32f1b3fce99765eab7d092bee9427106ad66224
Edwin Keijl <e.keijl@student.utwente.nl>
2010-04-26 18:06:25 +0200

    Comparison delete testcase en 404.html

--- beat/benchmarks/tests.py
+++ beat/benchmarks/tests.py
@@ -1,23 +1,27 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.test.client import Client
+from django.contrib.auth.models import User
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from beat.benchmarks.models import Comparison, ModelComparison, Tool, Algorithm
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+class MyComparisonDeleteTest(TestCase):
+	def setUp(self):
+		self.client = Client()
+		self.a = Algorithm.objects.create(name="TestAlgorithm")
+		self.t = Tool.objects.create(name="TestTool", version=1)
+		self.u = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+		self.c = Comparison.objects.create(user=self.u, benchmarks=1, date_time="01-01-2000 00:00:00")
+		self.m = ModelComparison.objects.create(user=self.u, type="states", tool=self.t, algorithm=self.a, date_time="01-01-2000 00:00:00")
+	
+	def test_delete(self):
+		# Delete the ModelComparison and check if the Comparison still exists (by counting all (Model)Comparison objects)
+		response = self.client.get('/user/compare/model/delete/1/')
+		print response.status_code
+		self.assertEqual(ModelComparison.objects.all().count(), 0)
+		self.assertEqual(Comparison.objects.all().count(),1)
 
->>> 1 + 1 == 2
-True
-"""}
+		# Now delete the remaining Comparison as well
+		response = self.client.get('/user/compare/delete/1/')
+		self.assertEqual(ModelComparison.objects.all().count(),0)
+		self.assertEqual(Comparison.objects.all().count(),0)
 
