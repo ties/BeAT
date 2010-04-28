8f6bced51e07b2a4eb5ba1328bc90370f71c387d
Namnatulco <rensvdheijden@gmail.com>
2010-04-28 23:53:15 +0200

    tested fully automated populate_db.sh, rough time estimate: 4 minutes for 1800 logs

--- beat/populate_db.sh
+++ beat/populate_db.sh
@@ -1,5 +1,15 @@
+date
 rm db.db
 python manage.py syncdb
 export DJANGO_SETTINGS_MODULE=settings
 python file_to_db.py
-python filereader.py --quiet ltsmin-ouput/output.txt
+python filereader.py --quiet ltsmin-ouput/output.txt > log0.txt~
+if [ ! -z $1 ]
+then
+	for i in $1/*; do
+		for j in $i/*; do
+			python filereader.py --quiet $* > log1.txt~
+		done
+	done
+fi
+date
