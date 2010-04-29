526739a734a4c01511719c0df3747cb7f19c4da9
Namnatulco <rensvdheijden@gmail.com>
2010-04-30 00:21:33 +0200

    dve2lts-grey logs now parse, a fraction is in the database (needs more testing). logconvert now takes extra argument

--- beat/logconvert.sh
+++ beat/logconvert.sh
@@ -1,10 +1,10 @@
 let counter=0
-if [ -d $1 ] && [ ! -z $1 ] &&	 ( [ ! -e ./tmp ] || ( [ ! -z $2 ] && [ ! -e $2 ]   ) )
+if [ -d $1 ] && [ ! -z $1 ] &&	 ( [ ! -e ./tmp ] || ( [ ! -z $2 ] && [ ! -e $2 ]   ) && [ ! -z $3 ])
 then
 	echo "working directory: $1"
 else
-	echo "invalid directory $1 or the ./tmp folder exists and the second argument points to something that exists or is not provided"
-	echo "please provide a valid directory and target, or delete ./tmp"
+	echo "invalid directory $1 or the ./tmp folder exists and $2 points to something that exists or is not provided, or tool/algortihm ($3) is not provided"
+	echo "please provide a valid directory and target, or delete ./tmp, and provide a valid tool/algorithm name (examples: dve2lts-grey, dve-reach)"
 	exit
 fi
 
@@ -36,7 +36,7 @@ for i in $f*; do
 			echo Processor: $(uname -p)
 			echo Memory-total: $(cat /proc/meminfo | grep MemTotal | tr -s " " | cut -d" " -f 2 -)
 			echo DateTime: 2010 03 16 13 24 43
-			echo Call: memtime dve-reach -c $i
+			echo Call: memtime $3 -c $i
 			cat $i
 			echo "REPORT ENDS HERE"
 		) > "$i.eatme"
