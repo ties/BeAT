5b2468cd5233d1474e36099e25f0d403f5801f7d
Thiez <Matthijs@.Astaroth>
2010-05-03 14:40:19 +0200

    Jobs meuk

--- beat/jobs/jobs.py
+++ beat/jobs/jobs.py
@@ -6,7 +6,7 @@ class JobGenerator:
 	
 	__directory = "."
 	
-	def __getSysInfo(self):
+	def __getSysInfo(self, toolname):
 		result = ""
 		result += "echo Nodename: $(uname -n)\n"
 		result += "echo Hardware-name: $(uname -m)\n"
@@ -18,6 +18,8 @@ class JobGenerator:
 		result += "echo Processor: $(uname -p)\n"
 		result += "echo Memory-total: $(cat /proc/meminfo | grep MemTotal | tr -s " " | cut -d\" \" -f 2 -\n"
 		result += "echo DateTime: $(date '+%Y %m %d %H %M %S') $[ $(date '+%N') / 1000]\n"
+		
+		result += "echo Tool version: $(" + toolname + " --version | cut -d' ' -f 2)\n"
 		return result
 	
 	def __makePBSHeader(self, filename, nodes):
@@ -35,16 +37,18 @@ class JobGenerator:
 		result += "ulimit -s 65536\n"
 		return result
 	
-	def pbsgen(self, filename, nodes, command):
+	def pbsgen(self, toolname, modelname, nodes, tooloptions):
 		"""Generates the jobs"""
 		result = ""
-		result += self.__makePBSHeader(filename, nodes)
+		result += self.__makePBSHeader(modelname, toolname, nodes)
 		result += "\n"
 		
 		result += "cd " + self.__directory + "\n"
 		result += self.__setStack()
 		result += "echo command: " + command + "\n"
 		result += self.__getSysInfo()
+		result += "\n"
+		result += "memtime " + toolname + " " + tooloptions + " " + modelname + "\n"
 		return result
 
 if __name__ == '__main__':
