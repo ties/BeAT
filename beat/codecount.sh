7d9256bdd5025a1d0f46384a3576708e1162f3cf
Thiez <Matthijs@.Astaroth>
2010-05-04 09:35:59 +0200

    Added CSS count

--- beat/codecount.sh
+++ beat/codecount.sh
@@ -1,7 +1,9 @@
 LOCPYTHON="$(cat $(find | grep \\.py) | wc -l)"
 LOCJS="$(cat $(find | grep \\.js | grep -v jquery | grep -v json) | wc -l)"
 LOCHTML="$(cat $(find | grep \\.html) | wc -l)"
+LOCCSS="$(cat $(find | grep \\.css) | wc -l)"
 echo LoC python: $LOCPYTHON
 echo LoC js: $LOCJS
 echo LoC html: $LOCHTML
-echo Total: $[ $LOCPYTHON + $LOCJS + $LOCHTML ]
+echo LoC css: $LOCCSS
+echo Total: $[ $LOCPYTHON + $LOCJS + $LOCHTML + $LOCCSS ]
