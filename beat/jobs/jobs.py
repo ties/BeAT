ac4c4fd0bb74fd5cb6211eb9e76bf16abcca39b1
Thiez <Matthijs@.Astaroth>
2010-05-07 14:54:04 +0200

    jobs.py: JobGenerator heeft methode om voor elk model dat hij kan vinden de goede .pbs-jes te genereren ^^

--- beat/jobs/jobs.py
+++ beat/jobs/jobs.py
@@ -1,8 +1,19 @@
 import sys
+import time
+import os
+
+class Job:
+	
+	def __init__(self, fileName, script):
+		self.name = fileName
+		self.script = script
+
 
 class JobGenerator:
 	
 	__directory = "."
+	jobs = []
+	
 	
 	def __getSysInfo(self, toolname, modelname, tooloptions):
 		result = ""
@@ -61,23 +72,54 @@ class JobGenerator:
 			result += "memtime " + toolname + " " + tooloptions + " " + modelname + " " + postfix + "\n"
 		return result
 	
-	__stdNodes = "1:E5335,walltime=4:00:00"
+	def jobgen(self, nodes, toolname, tooloptions, modelname, prefix="", postfix="", filename=None):
+		j= Job( filename+".pbs", self.pbsgen(nodes, toolname, tooloptions, modelname, prefix=prefix, postfix=postfix, filename=filename) )
+		self.jobs.append(j)
+		return j
+	
+	__ext2type = dict({'.lps': 'lps', '.tbf': 'lpo', '.b': 'nips', '.dve': 'dve'})
+	
+	def __extension_to_type(self, extension):
+		return self.__ext2type.get(extension)
 	
 	def suitegen(self, modelname):
-		base = modelname[:modelname.rfind(".")]
-		lang = modelname[modelname.rfind("."):]
 		
-		pbsgen( "1:E5335,walltime=4:00:00", lang+"2lts-grey", "", modelname, filename=base+"-"+lang+"-idx" )
-		pbsgen( "1:E5335,walltime=4:00:00", lang+"2lts-grey", "-cache", modelname, filename=base+"-"+lang+"-idx-cache" )
+		base = modelname[:modelname.rfind('.')]
+		lang = self.__extension_to_type(modelname[modelname.rfind('.'):])
+		
+		greytool = lang+"2lts-grey"
+		reachtool = lang+"-reach"
+		mpitool = lang+"2lts-mpi"
+		stdNodes = "1:E5335,walltime=4:00:00"
+		filenameBase = base + "-" + lang
+		
+		self.jobgen( stdNodes, greytool, "", modelname, filename=filenameBase+"-idx" )
+		self.jobgen( stdNodes, greytool, "-cache", modelname, filename=filenameBase+"-idx-cache" )
 		for vset in ["list", "tree", "fdd"]:
-			pbsgen( "1:E5225,walltime=4:00:00", lang+"2lts-grey", "--state vset --vset "+vset, modelname, filename=base+"-"+lang+"-"+vset )
-		pass
+			self.jobgen( stdNodes, greytool, "--state vset --vset "+vset, modelname, filename=filenameBase+"-"+vset )
+			self.jobgen( stdNodes, greytool, "--state vset --vset "+vset+" --cache", modelname, filename=filenameBase+"-"+vset+"-cache" )
+		for order in ["bfs", "bfs2", "chain"]:
+			for vset in ["list", "fdd"]:
+				self.jobgen( stdNodes, reachtool, "--order "+order+" --vset "+vset, modelname, filename=filenameBase+"-"+order+"-"+vset )
+		
+		for W in [1, 2, 4]:
+			mpiNodes = str(W)+":ppn=6:E5335,walltime=4:00:00"
+			self.jobgen( mpiNodes, mpitool, "", modelname, filename=filenameBase+"-mpi-"+str(W)+"-6", prefix="mpirun -mca btl tcp,self" )
+			self.jobgen( mpiNodes, mpitool, "--cache", modelname, filename=filenameBase+"-mpi-cache-"+str(W)+"-6", prefix="mpirun -mca btl tcp,self" )
 		
 	def generate_all(self):
-		pass
+		contents = os.listdir('.')
+		for f in contents:
+			if os.path.isfile(f) and os.path.splitext(f)[1] in self.__ext2type:
+				self.suitegen(os.path.basename(f))
 		
 
 if __name__ == '__main__':
 	j = JobGenerator()
-	print j.pbsgen("1", "lpo2lts-grey", "-cache", "dphil-10.tbf")
+	#print j.pbsgen("1", "lpo2lts-grey", "-cache", "dphil-10.tbf")
+	#time.sleep(3)
+	#j.suitegen("dphil-10.tbf")
+	j.generate_all()
+	for job in j.jobs:
+		print job.name
 	sys.exit(0)
