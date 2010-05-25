9370f0eeaf012215a6c462dace9b452d4044b470
Thiez <thiezz@gmail.com>
2010-05-25 11:44:32 +0200

    Deze zou het nu ook moeten doen

---
+++ populate_db.sh
@@ -0,0 +1,18 @@
+python manage.py syncdb
+export DJANGO_SETTINGS_MODULE=beat.settings
+
+rm -rf logs
+rm -rf git
+mkdir logs
+mkdir git
+mkdir git/ltsmin
+
+python db_defaults.py
+python filereader.py --noisy ltsmin-ouput/output.txt > log0.txt~
+python spawndata.py
+python spawndata.py
+python spawndata.py
+if [ ! -z $1 ]
+then
+	python filereader.py --noisy $* > log1.txt~
+fi
