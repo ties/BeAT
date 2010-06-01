97b2be56c3895c9705e3cf4ebb7ca975c9e6243b
Edwin Keijl <e.keijl@student.utwente.nl>
2010-06-01 21:34:56 +0200

    can has fix?

--- beat/comparisons/urls.py
+++ beat/comparisons/urls.py
@@ -13,9 +13,6 @@ urlpatterns = patterns('beat.comparisons.views',
 	# Graph image for the model-comparison
 	(r'^tool/(?P<id>\d+)/scatterplot.png$', 'scatterplot'),
 	(r'^model/(?P<id>\d+)/benchmark.png$', 'graph_model'),
-	
-	# For excluding IDs on a model comparison, work in progress
-	(r'^model/(?P<id>\d+)/exclude$', 'exclude_ids'),
 
 	# Exporting graphs
 	url (r'^tool/(?P<id>\d+)/export$', 'export_graph',name="export_benchmark_graph"),
