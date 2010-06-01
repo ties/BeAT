b191df58917e71bf173046461a6553a58b504ffd
Rens vd Heijden <rensvdheijden@gmail.com>
2010-06-01 21:36:15 +0200

    deleted some old templates, added fixture stuff

--- beat/comparisons/urls.py
+++ beat/comparisons/urls.py
@@ -14,9 +14,6 @@ urlpatterns = patterns('beat.comparisons.views',
 	(r'^tool/(?P<id>\d+)/scatterplot.png$', 'scatterplot'),
 	(r'^model/(?P<id>\d+)/benchmark.png$', 'graph_model'),
 	
-	# For excluding IDs on a model comparison, work in progress
-	(r'^model/(?P<id>\d+)/exclude$', 'exclude_ids'),
-
 	# Exporting graphs
 	url (r'^tool/(?P<id>\d+)/export$', 'export_graph',name="export_benchmark_graph"),
 	url (r'^model/(?P<id>\d+)/export$', 'export_graph', {'model': True}, name="export_model_graph"),
