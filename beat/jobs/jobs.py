45c6edddda4243749420404acd4bf5429160c0ed
Thiez <thiezz@gmail.com>
2010-06-03 17:48:14 +0200

    Job generation now relies completely on the templates.

--- beat/jobs/jobs.py
+++ beat/jobs/jobs.py
@@ -19,70 +19,10 @@ class JobGenerator:
 	def pbsgen(self, nodes, toolname, tooloptions, modelname, prefix="", postfix="", filename=""):
 		if not filename:
 			filename = toolname
-		t = Template( open('jTemplate.tpl','r').read() )
-		#t = get_template('jobs/jTemplate.tpl')
+		#t = Template( open(templatepath).read() )
+		t = loader.get_template('jTemplate.tpl')
 		c = Context({"nodes": nodes, "toolname": toolname, "tooloptions": tooloptions, "modelname":modelname, "prefix":prefix, "postfix":postfix, "filename":filename})
 		result = t.render(c);
-		print result
-		
-	
-	def __getSysInfo(self, toolname, modelname, tooloptions):
-		result = ""
-		
-		result += "echo BEGIN OF HEADER\n"
-		
-		result += "echo Nodename: $(uname -n)\n"
-		result += "echo Hardware-name: $(uname -m)\n"
-		result += "echo OS: $(uname -o)\n"
-		result += "echo Kernel-name: $(uname -s)\n"
-		result += "echo Kernel-release: $(uname -r)\n"
-		result += "echo Kernel-version: $(uname -v)\n"
-		result += "echo Hardware-platform: $(uname -i)\n"
-		result += "echo Processor: $(uname -p)\n"
-		result += "echo Memory-total: $(cat /proc/meminfo | grep MemTotal | tr -s \" \" | cut -d\" \" -f 2 -)\n"
-		result += "echo DateTime: $(date '+%Y %m %d %H %M %S') $[ $(date '+%-N') / 1000 ]\n"
-		
-		result += "echo ToolVersion: $(" + toolname + " --version | cut -d' ' -f 2)\n"
-		result += "echo Call: " + toolname + " " + tooloptions + " " + modelname + "\n"
-		
-		result += "echo END OF HEADER\n"
-		return result
-	
-	def __makePBSHeader(self, filename, nodes):
-		result = ""
-		result += "#PBS -q default\n"
-		result += "#PBS -N beat_" + filename + "\n"
-		result += "#PBS -l nodes=" + nodes + "\n"
-		result += "#PBS -j oe\n"
-		result += "#PBS -W x=NACCESSPOLICY:SINGLEJOB\n"
-		return result
-	
-	def __setStack(self):
-		result = ""
-		result += "echo Setting stack to 64MB\n"
-		result += "ulimit -s 65536\n"
-		return result
-		
-	def _pbsgen(self, nodes, toolname, tooloptions, modelname, prefix="", postfix="", filename=None):
-		"""Generates the jobs"""
-		result = ""
-		if filename is None:
-			result += self.__makePBSHeader(toolname, nodes)
-		else:
-			result += self.__makePBSHeader(filename, nodes)
-		result += "\n"
-		
-		result += "cd " + self.__directory + "\n"
-		result += self.__setStack()
-		result += "echo command: " + prefix + " memtime " + toolname + " " + tooloptions + " " + modelname + "\n"
-		result += self.__getSysInfo(toolname, modelname, tooloptions)
-		result += "\n"
-		if prefix:
-			result += prefix + " memtime " + toolname + " " + tooloptions + " " + modelname + " " + postfix + "\n"
-		else:
-			result += "memtime " + toolname + " " + tooloptions + " " + modelname + " " + postfix + "\n"
-		result += "\n"
-		result += "echo REPORT ENDS HERE\n"
 		return result
 	
 	def jobgen(self, nodes, toolname, tooloptions, modelname, prefix="", postfix="", filename=None):
--- beat/jobs/views.py
+++ beat/jobs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response, redirect, get_object_or_404, get_list_or_404
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
+from django.http import HttpResponse
 
 from beat.jobs.forms import *
 
