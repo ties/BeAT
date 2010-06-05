8b032bbeb3d393d9fe1c0b9e91e891b054e3108f
Rens vd Heijden <rensvdheijden@gmail.com>
2010-06-06 01:05:06 +0200

    01:04  * namnatulco heeft het bovenstaande option spul erin gehackt, wat aan de output gewerkt en een --dulwich switch gemaakt

--- beat/tools/logsave.py
+++ beat/tools/logsave.py
@@ -22,41 +22,43 @@ class GitFileError(Exception):
 		else:
 			return "Error occured: %s."%(error)
 
-# initialize the repo if it doesn't exists, or load it if it does
-if not path.exists(LOGS_PATH):
-	print "creating folder "+ LOGS_PATH
-	mkdir(LOGS_PATH)
-	repo = Repo.init(LOGS_PATH)
-	blob = Blob.from_string("data")
-	tree =Tree()
-	tree.add(0100644, "initfile", blob.id)
-	c = Commit()
-	c.tree = tree.id
-	author = "Writer a@a.com"
-	c.author=c.committer=author
-	c.commit_time=c.author_time=int(time())
-	tz = parse_timezone('+0200')
-	c.commit_timezone=c.author_timezone=tz
-	c.encoding="UTF-8"
-	c.message="initial commit"
-	store = repo.object_store
-	store.add_object(blob)
-	store.add_object(tree)
-	store.add_object(c)
-	repo.refs['refs/heads/master'] = c.id
-	repo.refs['HEAD'] = 'ref: refs/heads/master'
-	print "success!"
-else:
-	#this is how to create a Repo object from an existing repository
-	from dulwich.errors import NotGitRepository
-	try:
-		repo = Repo(LOGS_PATH)
-	except NotGitRepository as e:
-		raise GitFileError("Error: the path %s exists but is not a git repository."%LOGS_PATH)
+def __init_code__():
+	# initialize the repo if it doesn't exists, or load it if it does
+
+	if not path.exists(LOGS_PATH):
+		print "creating folder "+ LOGS_PATH
+		mkdir(LOGS_PATH)
+		repo = Repo.init(LOGS_PATH)
+		blob = Blob.from_string("data")
+		tree =Tree()
+		tree.add(0100644, "initfile", blob.id)
+		c = Commit()
+		c.tree = tree.id
+		author = "Writer a@a.com"
+		c.author=c.committer=author
+		c.commit_time=c.author_time=int(time())
+		tz = parse_timezone('+0200')
+		c.commit_timezone=c.author_timezone=tz
+		c.encoding="UTF-8"
+		c.message="initial commit"
+		store = repo.object_store
+		store.add_object(blob)
+		store.add_object(tree)
+		store.add_object(c)
+		repo.refs['refs/heads/master'] = c.id
+		repo.refs['HEAD'] = 'ref: refs/heads/master'
+		print "success!"
+	else:
+		#this is how to create a Repo object from an existing repository
+		from dulwich.errors import NotGitRepository
+		try:
+			repo = Repo(LOGS_PATH)
+		except NotGitRepository as e:
+			raise GitFileError("Error: the path %s exists but is not a git repository."%LOGS_PATH)
+	return repo
 
 #####static variables
 default_perms = 0100644
-store=repo.object_store
 
 #####functions of this module
 def get_latest_tree(repo):
@@ -67,7 +69,7 @@ def get_latest_tree(repo):
     tree_id = commit.tree
     return repo.tree(tree_id)
 
-def create_log(contents, filename, overwrite=False):
+def create_log(repo, contents, filename, overwrite=False):
 	"""creates a log with the specified content.
 	This function creates a log file in the git repository specified by LOGS_PATH in settings.py.
 	If a file with name filename exists, False is returned, unless overwrite=True.
@@ -83,6 +85,8 @@ def create_log(contents, filename, overwrite=False):
 	
 	if not contents:
 		return "Error: empty contents"
+	if type(contents) is list:
+		contents=''.join(contents)
 
 	if path.exists(path.join(LOGS_PATH, filename)) and not overwrite:
 		return "Error: file exists, overwrite not specified"
@@ -102,6 +106,7 @@ def create_log(contents, filename, overwrite=False):
 	commit.encoding = "UTF-8"
 	commit.message = "Writing log file %s"%(filename)
 	
+	store=repo.object_store
 	store.add_object(blob)
 	store.add_object(tree)
 	store.add_object(commit)
@@ -110,7 +115,7 @@ def create_log(contents, filename, overwrite=False):
 
 	return True
 
-def get_log(filename):
+def get_log(repo, filename):
 	"""reads and returns the specified log
 	This function fetches a log specified by filename, reads it and returns it as a single string.
 	This function will always read from the HEAD commit.
@@ -131,4 +136,5 @@ def get_log(filename):
 	except Exception as e:
 		raise GitFileError(e)
 
-
+#run the init code, if this module is ran directly
+__init_code__()
