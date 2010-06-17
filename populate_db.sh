b1ec4c3f370538afb9aa4baaa901f4e9e1a51d63
Rens vd Heijden <rensvdheijden@gmail.com>
2010-06-17 23:07:55 +0200

    db_defaults can now be called for different version strings, might update with a list of models and make it more pretties

--- populate_db.sh
+++ populate_db.sh
@@ -7,19 +7,21 @@ rm -rf beat/logs
 rm -rf beat/git
 mkdir beat/logs
 mkdir beat/git
-mkdir beat/git/ltsmin
+#mkdir beat/git/ltsmin
 
 echo "... adding basic data..."
 python db_defaults.py
 echo "... reading test logs..."
-python filereader.py --noisy ltsmin-output/output.txt > log0.txt~
-echo "... generating random data..."
-python spawndata.py
-python spawndata.py
-python spawndata.py
+python filereader.py --noisy ltsmin-output/output.txt
+# > log0.txt~
+#echo "... generating random data..."
+#python spawndata.py
+#python spawndata.py
+#python spawndata.py
 if [ ! -z $1 ]
 then
 	echo "... reading provided logs..."
-	python filereader.py --noisy $* > log1.txt~
+	python filereader.py --noisy $*
+# > log1.txt~
 fi
 echo "... done!"
